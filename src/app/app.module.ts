436566da88b38a057bd965423881dbefc166f2ae
Florian Spier <Florian.Spier@portofantwerp.com>
2021-11-21 20:45:14 +0100

    Mini Rx with Angular Integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterShellComponent } from './counter-shell/counter-shell.component';
+import { EffectsModule, StoreModule } from 'mini-rx-store-ng';
+import { Action } from 'mini-rx-store';
+
+interface CounterState {
+    count: number;
+}
+
+const counterInitialState: CounterState = {
+    count: 1
+};
+
+function counterReducer(
+    state: CounterState = counterInitialState,
+    action: Action
+): CounterState {
+    switch (action.type) {
+        case 'inc':
+            return {
+                ...state,
+                count: state.count + 1
+            };
+        default:
+            return state;
+    }
+}
+
+export class TodoEffects {}
 
 @NgModule({
     declarations: [
         AppComponent,
-        CounterShellComponent
+        CounterShellComponent,
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        StoreModule.forRoot({reducers: {counter: counterReducer}}),
+        EffectsModule.register([TodoEffects])
     ],
     providers: [],
     bootstrap: [AppComponent]
